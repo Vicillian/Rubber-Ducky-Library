6590c29c9b170eb24a037807c42951be6a8cac33
Glitch (pentagonal-illustrious-question) <none>
2020-05-14 01:29:11 +0000

    ⛹🎻 Updated with Glitch

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 $(".searchButton").click(function() {
- $("body").append("<p>show</p>");
+      $(".searchResult").empty();
   var bookInput = $(".bookInput").val();
-  var newBookAPI = "https://www.googleapis.com/books/v1/volumes?q=" + bookInput;
+  var filter1 = $("#filter1").prop("checked");
+  var filter2 = $("#filter2").prop("checked");
+  var filter3 = $("#filter3").prop("checked");
+  if ( filter1 === true) {
+    var newBookAPI = "https://www.googleapis.com/books/v1/volumes?startIndex=0&maxResults=10&q=" + bookInput + "&filter=ebooks";
+  } else if ( filter2 === true) {
+    var newBookAPI = "https://www.googleapis.com/books/v1/volumes?startIndex=0&maxResults=10&q=" + bookInput + "&filter=free-ebooks";
+  } else if ( filter3 === true) {
+    var newBookAPI = "https://www.googleapis.com/books/v1/volumes?startIndex=0&maxResults=10&q=" + bookInput + "&filter=paid-ebooks";
+  } else {
+    var newBookAPI = "https://www.googleapis.com/books/v1/volumes?startIndex=0&maxResults=10&q=" + bookInput;
+  }
   console.log(newBookAPI);
   fetch(newBookAPI)
 .then(function(response) {
@@ -9,20 +20,37 @@ $(".searchButton").click(function() {
       })
       .then(function(data) {
         // function body
+      
         data.items.forEach(function(book){
-          $(".listbar").append(`
+          
+          var i;
+          var authors = "";
+          for (i = 0; i < book.volumeInfo.authors.length; i++) {
+            var authorIndex = book.volumeInfo.authors[i];
+            console.log(authorIndex);
+            if (i !== book.volumeInfo.authors.length - 1) {
+              authors = authors + authorIndex + ", ";
+            } else {
+              authors = authors + authorIndex;
+            }
+          }
+        
+          $(".searchResult").append(`
           <div class="bookItems">
-            <p>${book.volumeInfo.title}</p>
-            <p>${book.volumeInfo.author}</p>
-          </div>`)
+            <div class="bookImgPart">
+              <img class="bookImg" src=${book.volumeInfo.imageLinks.smallThumbnail}>
+            </div>
+            <div class="bookInfo">
+              <p>${book.volumeInfo.title}</p>
+              <p>${authors}</p>
+            </div>
+          </div>
+          
+          `)
         });
         
       });
 }); 
 
 
-// Vic- Imm really bad at api but I'll try I guess
-
-// were you able to look at the data that's coming back here? https://www.googleapis.com/books/v1/volumes?q=harry%20potter
-// in a new tab
-// 
+// The Sun is FLAT
